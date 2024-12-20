a447f7d26f2380b88ff97cfdb112657a99b47fdd
LionOfPanjshir <a.higbee@gmail.com>
2024-12-20 02:21:09 -0700

    Initial Commit

---
+++ AubioBPMDetector/AubioBPMDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AubioBPMDetectorApp.swift
+//  AubioBPMDetector
+//
+//  Created by Andrew Higbee on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct AubioBPMDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
